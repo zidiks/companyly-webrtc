a87b839268e881d5e8511c0330580969492aad30
zidiks <zidiks228@gmail.com>
2023-10-30 13:06:21 +0300

    upd: add CI/CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   host: process.env.API_HOST,
-  syncPort: process.env.HTTP_PORT,
+  syncPort: process.env.SYNC_PORT,
   socketPort: process.env.SOCKET_PORT,
   peerPort: process.env.PEER_PORT,
 };
